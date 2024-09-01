4c3b7914fd8041410066677da0d528ebdc0f6e81
João Pedro Rodrigues <joaop.rfreitas05@gmail.com>
2024-09-01 14:36:03 -0300

    ok

--- pitiquinho/src/main/java/br/loja/pitiquinho/controller/ListarUsuariosBackoffice.java
+++ pitiquinho/src/main/java/br/loja/pitiquinho/controller/ListarUsuariosBackoffice.java
@@ -1,13 +1,24 @@
 package br.loja.pitiquinho.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import br.loja.pitiquinho.service.UsuarioService;
+import br.loja.pitiquinho.model.Usuario;
 
 @Controller
 public class ListarUsuariosBackoffice {
 
+    @Autowired
+    private UsuarioService usuario;
+
+
     @GetMapping("/listarusuariobackoffice")
     public String listarusuariobackoffice (){
+        List<Usuario> = usuario.listarUsuarios();
+        
         return "listarusuariobackoffice";
     }
+
+    
 }
