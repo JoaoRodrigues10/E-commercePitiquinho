a7a1328562fa53ddbf2253929523ab8ee661e509
Matheus Rafael <90002960+MatheusRafaDev@users.noreply.github.com>
2024-10-31 02:00:29 -0300

    fazendo as ultimas alterações

---
+++ pitiquinho/src/main/java/br/loja/pitiquinho/controller/EnderecoController.java
@@ -0,0 +1,143 @@
+package br.loja.pitiquinho.controller;
+
+import br.loja.pitiquinho.model.Endereco;
+import br.loja.pitiquinho.model.Usuario;
+import br.loja.pitiquinho.service.EnderecoService;
+import br.loja.pitiquinho.service.UsuarioService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/endereco")
+public class EnderecoController {
+
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @RequestMapping("/buscar-endereco")
+    @ResponseBody
+    public ResponseEntity<String> buscarEndereco(@RequestParam String cep) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "https://viacep.com.br/ws/" + cep + "/json/";
+            return ResponseEntity.ok(restTemplate.getForObject(url, String.class));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("{\"error\": \"Erro ao buscar endereço.\"}");
+        }
+    }
+
+    @GetMapping("/cadastro")
+    public String mostrarCadastroEndereco(@RequestParam("usuarioId") Long usuarioId, Model model, HttpSession session) {
+        Usuario usuario = usuarioService.findById(usuarioId);
+        List<Endereco> enderecos = enderecoService.buscarEnderecosPorUsuarioId(usuarioId);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("enderecos", enderecos);
+        model.addAttribute("endereco", new Endereco());
+        return "endereco";
+    }
+
+    @PostMapping("/cadastro-endereco")
+    public String cadastrarEndereco(@Valid @ModelAttribute("endereco") Endereco endereco,
+                                    BindingResult result, HttpSession session, Model model) {
+
+        Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
+
+        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
+            return "redirect:/login";
+        }
+
+        endereco.setUsuarioId(usuarioLogado.getId());
+
+        if (result.hasErrors()) {
+            return "redirect:/endereco/cadastro?usuarioId=" + usuarioLogado.getId();
+        }
+
+        enderecoService.salvarEndereco(endereco);
+
+        return "redirect:/usuario/editar/" + usuarioLogado.getId();
+    }
+
+    @GetMapping("/cadastro-faturamento")
+    public String mostrarCadastroEnderecoFaturamento(@RequestParam("usuarioId") Long usuarioId, Model model, HttpSession session) {
+        Usuario usuario = usuarioService.findById(usuarioId);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("endereco", new Endereco());
+        return "endereco-faturamento";
+    }
+
+    @PostMapping("/cadastro-endereco-faturamento")
+    public String cadastrarEnderecoFaturamento(@Valid @ModelAttribute("endereco") Endereco endereco,
+                                    BindingResult result, HttpSession session, Model model) {
+
+        Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
+
+        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
+            return "redirect:/login";
+        }
+
+        endereco.setUsuarioId(usuarioLogado.getId());
+
+        if (result.hasErrors()) {
+            return "redirect:/endereco/cadastro?usuarioId=" + usuarioLogado.getId();
+        }
+
+        enderecoService.salvarEndereco(endereco);
+
+        return "redirect:/endereco/editar/" + usuarioLogado.getId();
+    }
+
+
+    @GetMapping("/editar/{usuarioId}")
+    public String listarEnderecos(@PathVariable Long usuarioId, Model model, HttpSession session) {
+        Usuario usuario = usuarioService.findById(usuarioId);
+        Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
+
+        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
+            return "redirect:/login";
+        }
+
+        List<Endereco> enderecos = enderecoService.buscarEnderecosPorUsuarioId(usuarioLogado.getId());
+
+        boolean temEnderecoFaturamento = enderecos.stream()
+                .anyMatch(endereco -> "Faturamento".equals(endereco.getTipoEndereco()));
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("enderecos", enderecos);
+        model.addAttribute("endereco", new Endereco());
+        model.addAttribute("temEnderecoFaturamento", temEnderecoFaturamento);
+
+        return "endereco";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirEndereco(@PathVariable Long id, RedirectAttributes redirectAttributes, HttpSession session) {
+        Endereco endereco = enderecoService.findById(id);
+
+        Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
+        if (usuarioLogado == null) {
+            return "redirect:/login";
+        }
+
+        if ("Faturamento".equalsIgnoreCase(endereco.getTipoEndereco())) {
+            return "redirect:/endereco/editar/" + usuarioLogado.getId();
+        }
+
+        enderecoService.excluirEndereco(id);
+        redirectAttributes.addFlashAttribute("successMessage", "Endereço excluído com sucesso.");
+        return "redirect:/endereco/editar/" + usuarioLogado.getId();
+    }
+
+}
--- pitiquinho/src/main/java/br/loja/pitiquinho/controller/UsuarioClienteController.java
+++ pitiquinho/src/main/java/br/loja/pitiquinho/controller/UsuarioClienteController.java
@@ -1,27 +1,16 @@
 package br.loja.pitiquinho.controller;
 
-import br.loja.pitiquinho.model.Endereco;
 import br.loja.pitiquinho.model.Usuario;
-import br.loja.pitiquinho.service.EnderecoService;
 import br.loja.pitiquinho.service.UsuarioService;
 import br.loja.pitiquinho.util.util;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/usuario")
@@ -31,22 +20,7 @@ public class UsuarioClienteController {
     private UsuarioService usuarioService;
 
     @Autowired
-    private EnderecoService enderecoService;
-
-    @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-
-    @RequestMapping("/buscar-endereco")
-    @ResponseBody
-    public ResponseEntity<String> buscarEndereco(@RequestParam String cep) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            String url = "https://viacep.com.br/ws/" + cep + "/json/";
-            return ResponseEntity.ok(restTemplate.getForObject(url, String.class));
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("{\"error\": \"Erro ao buscar endereço.\"}");
-        }
-    }
 
     @GetMapping("/cadastro")
     public String mostrarFormularioCadastro(Model model) {
@@ -54,11 +28,9 @@ public class UsuarioClienteController {
         return "cadastro-usuario";
     }
 
-
     @PostMapping("/cadastro")
     public String cadastrarUsuario(@Valid @ModelAttribute("usuario") Usuario usuario,
-                                   BindingResult result, Model model,HttpSession session) {
-
+                                   BindingResult result, Model model, HttpSession session) {
 
         if (result.hasErrors() || !validarCadastro(usuario, result)) {
             return "cadastro-usuario";
@@ -68,116 +40,40 @@ public class UsuarioClienteController {
         usuario.setGrupo("Cliente");
         usuarioService.criarUsuario(usuario);
 
-        model.addAttribute("usuario",  usuario);
+        model.addAttribute("usuario", usuario);
         session.setAttribute("usuario", usuario);
 
-        return "redirect:/usuario/endereco?usuarioId=" + usuario.getId();
-
+        return "redirect:/endereco/cadastro-faturamento?usuarioId=" + usuario.getId();
     }
-
-
-    @GetMapping("/endereco")
-    public String mostrarCadastroEndereco(@RequestParam("usuarioId") Long usuarioId, Model model,HttpSession session) {
-        Usuario usuario = usuarioService.findById(usuarioId);
-        List<Endereco> enderecos = enderecoService.buscarEnderecosPorUsuarioId(usuarioId);
-        model.addAttribute("usuario", usuario);
-        model.addAttribute("enderecos", enderecos);
-        model.addAttribute("endereco", new Endereco());
-
-        return "endereco";
-    }
-
-    @PostMapping("/cadastro-endereco")
-    public String cadastrarEndereco(@Valid @ModelAttribute("endereco") Endereco endereco,
-        BindingResult result, HttpSession session, Model model) {
-
-        Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
-
-
-        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
-            return "redirect:/login";
-        }
-
-
-        endereco.setUsuarioId(usuarioLogado.getId());
-
-        if (result.hasErrors()) {
-            return "redirect:/usuario/endereco?usuarioId=" + usuarioLogado.getId();
-        }
-
-        enderecoService.salvarEndereco(endereco);
-
-        //return "redirect:/usuario/editar/" + usuarioLogado.getId();
-
-        return "redirect:/";
-    }
-
-    @GetMapping("/enderecos/{usuarioId}")
-    public String listarEnderecos(@PathVariable Long usuarioId, Model model, HttpSession session) {
-        Usuario usuario = usuarioService.findById(usuarioId);
-        Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
-
-        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
-            return "redirect:/login";
-        }
-
-        List<Endereco> enderecos = enderecoService.buscarEnderecosPorUsuarioId(usuarioLogado.getId());
-
-
-        boolean temEnderecoFaturamento = enderecos.stream()
-                .anyMatch(endereco -> "Faturamento".equals(endereco.getTipoEndereco()));
-
-        model.addAttribute("usuario", usuario);
-        model.addAttribute("enderecos", enderecos);
-        model.addAttribute("endereco", new Endereco());
-        model.addAttribute("temEnderecoFaturamento", temEnderecoFaturamento);
-
-        return "endereco";
-    }
-
-
-
-    @GetMapping("/excluir-endereco/{id}")
-    public String excluirEndereco(@PathVariable Long id, RedirectAttributes redirectAttributes) {
-        Endereco endereco = enderecoService.findById(id);
-
-        if ("Faturamento".equalsIgnoreCase(endereco.getTipoEndereco())) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Não é permitido apagar o endereço de faturamento.");
-            return "redirect:/enderecos"; // redireciona para a página de endereços
-        }
-
-        enderecoService.excluirEndereco(id);
-        redirectAttributes.addFlashAttribute("successMessage", "Endereço excluído com sucesso.");
-        return "redirect:/enderecos"; // redireciona para a página de endereços
-    }
-
-
-
-
 
     @GetMapping("/editar/{id}")
-    public String mostrarFormularioEdicao( Model model,HttpSession session) {
-
+    public String mostrarFormularioEdicao(Model model, HttpSession session) {
         Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
+
+        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
+            return "redirect:/login";
+        }
 
         model.addAttribute("usuario", usuarioLogado);
         session.setAttribute("usuario", usuarioLogado);
 
-        if (usuarioLogado == null || usuarioLogado.getGrupo() == null || usuarioLogado.getGrupo().isEmpty()) {
-            return "redirect:/login";
-        }
-
-        System.out.println(usuarioLogado.getId());
         return "editar-usuario";
     }
 
-
     @PostMapping("/editar/{id}")
     public String editarUsuario(@PathVariable Long id, @Valid @ModelAttribute("usuario") Usuario usuario,
-        BindingResult result, Model model, HttpSession session) {
+                                BindingResult result, Model model, HttpSession session) {
 
+        // Verifica se houve erros de validação inicial
         if (result.hasErrors()) {
+            model.addAttribute("usuario", usuario);
             return "editar-usuario";
+        }
+
+        // Valida cadastro do usuário
+        if (!validarCadastro(usuario, result)) {
+            model.addAttribute("usuario", usuario);
+            return "editar-usuario"; // Retorna à página de edição se houver erros de validação
         }
 
         Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
@@ -186,6 +82,7 @@ public class UsuarioClienteController {
             return "redirect:/login";
         }
 
+        // Se a senha não for preenchida, mantém a senha existente
         if (!usuario.getSenha().isEmpty()) {
             usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         } else {
@@ -196,12 +93,10 @@ public class UsuarioClienteController {
         usuario.setGrupo(usuarioLogado.getGrupo());
         usuarioService.atualizarUsuario(usuario);
 
-        model.addAttribute("usuario", usuario);
+        // Atualiza a sessão com as informações do usuário
         session.setAttribute("usuario", usuario);
-
-        return "redirect:/usuario/detalhes/" + usuario.getId();
+        return "redirect:/usuario/editar/" + usuario.getId();
     }
-
 
     private boolean validarCadastro(Usuario usuario, BindingResult result) {
         if (usuarioService.existsByEmail(usuario.getEmail())) {
@@ -218,11 +113,4 @@ public class UsuarioClienteController {
         }
         return true;
     }
-
-    private boolean isNomeValido(String nome) {
-        String[] partes = nome.split(" ");
-        return partes.length >= 2 && partes.length > 0 && partes[0].length() >= 3;
-    }
-
 }
-
